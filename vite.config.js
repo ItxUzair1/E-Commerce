109be334e558e50fe67903350fa192d19e90f027
ItxUzair1 <muhammaduzair1411@gmail.com>
2024-09-06 11:19:48 +0500

    All Changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // Update the base path to match your GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/E-Commerce/',  // This should match your repository name
+  base: '/e-commerce/',  // This should match your repository name
 });
